3d84628163ccfc74ee4ced166886b792b4888aa3
huzch <huzch123@gmail.com>
2024-12-08 23:32:32 +0800

    增加lazy插件的配置

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 # 符号链接，覆盖配置文件
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/nvim ~/.config/nvim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
