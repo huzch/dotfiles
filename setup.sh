671fceb98e7a56989ecbc47a56c91d196a03fbb5
huzch <huzch123@gmail.com>
2024-12-09 10:14:06 +0800

    更正nvim链接的错误

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # 符号链接，覆盖配置文件
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/dotfiles/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
