d3c1290d1ef6e6bf3203588454bfd072c6f5afe0
huzch <huzch123@gmail.com>
2024-12-27 10:38:52 +0800

    小改动

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -6,7 +6,7 @@ if not vim.g.vscode then
 	table.insert(plugins, require('plugins.ui'))
 	table.insert(plugins, require('plugins.move'))
 	table.insert(plugins, require('plugins.lsp'))
-	table.insert(plugins, require('plugins.dap'))
+	-- table.insert(plugins, require('plugins.dap'))
 end
 
 return plugins
