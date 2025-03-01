8df8a673b3a46d985f691b4d90c3ddeab1fc2317
huzch <huzch123@gmail.com>
2025-03-02 00:01:12 +0800

    实现nvim-dap可视化调试

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,76 +1,105 @@
 return {
 	{
-		'mfussenegger/nvim-dap',
-		ft = 'cpp',
+		"mfussenegger/nvim-dap",
+		event = "VeryLazy",
 		config = function()
-			local dap = require('dap')
+			local dap = require("dap")
+			vim.keymap.set("n", "<F5>", dap.continue, { noremap = true, silent = true })
+			vim.keymap.set("n", "<F10>", dap.step_over, { noremap = true, silent = true })
+			vim.keymap.set("n", "<F11>", dap.step_into, { noremap = true, silent = true })
+			vim.keymap.set("n", "<F12>", dap.step_out, { noremap = true, silent = true })
+			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { noremap = true, silent = true })
 
-			dap.adapters.gdb = {
-				type = 'executable',
-				command = 'gdb',
-				args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
-			}
+			vim.fn.sign_define("DapBreakpoint", {
+				text = "",
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			})
 
-			dap.configurations.cpp = {
-				{
-					name = 'Debug C++',
-					type = 'gdb',
-					request = 'launch',
-					program = function()
-						return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-					end,
-					cwd = '${workspaceFolder}',
-				},
-			}
+			vim.fn.sign_define("DapBreakpointRejected", {
+				text = "", -- or "❌"
+				texthl = "DiagnosticSignError",
+				linehl = "",
+				numhl = "",
+			})
 
-			vim.keymap.set('n', '<F5>', dap.continue, { noremap = true, silent = true })
-			vim.keymap.set('n', '<F10>', dap.step_over, { noremap = true, silent = true })
-			vim.keymap.set('n', '<F11>', dap.step_into, { noremap = true, silent = true })
-			vim.keymap.set('n', '<F12>', dap.step_out, { noremap = true, silent = true })
-			vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { noremap = true, silent = true })
-			vim.keymap.set('n', '<leader>r', dap.repl.toggle, { noremap = true, silent = true })
+			vim.fn.sign_define("DapStopped", {
+				text = "", -- or "→"
+				texthl = "DiagnosticSignWarn",
+				linehl = "Visual",
+				numhl = "DiagnosticSignWarn",
+			})
 		end,
 	},
 
 	{
-		'rcarriga/nvim-dap-ui',
-		ft = 'cpp',
+		"rcarriga/nvim-dap-ui",
+		event = "VeryLazy",
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 		config = function()
-			local dapui = require('dapui')
+			local dapui = require("dapui")
 			dapui.setup({
 				layouts = {
 					{
 						elements = {
-							{ id = "watches", size = 0.7 }, -- 监视窗口占右侧的上半部分
-							-- { id = "repl",    size = 0.3 }, -- REPL 窗口占右侧的下半部分
+							{ id = "console", size = 0.3 }, -- 控制台占右侧的下半部分
+							{ id = "watches", size = 0.7 }  -- 监视窗口占右侧的上半部分
 						},
-						size = 40,      -- 设置右侧总宽度，单位是列数
-						position = "right", -- 将布局放在右侧
+						size = 40,      -- 设置右侧总宽度
+						position = "right",
 					},
 				},
 			})
 
 			local dap = require("dap")
-			-- 在启动调试会话前打开 UI
-			dap.listeners.before['event_initialized']['dapui_config'] = function()
+			dap.listeners.after.event_initialized["dapui_config"] = function()
 				dapui.open()
 			end
-
-			-- 在调试会话启动前打开 UI
-			dap.listeners.before['event_launch']['dapui_config'] = function()
-				dapui.open()
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close()
 			end
-
-			-- 在调试会话终止时关闭 UI
-			dap.listeners.before['event_terminated']['dapui_config'] = function()
+			dap.listeners.before.event_exited["dapui_config"] = function()
 				dapui.close()
 			end
 
-			-- 在调试会话退出时关闭 UI
-			dap.listeners.before['event_exited']['dapui_config'] = function()
-				dapui.close()
-			end
+			vim.keymap.set('n', '<leader>c', function()
+				require("dapui").float_element("console", { enter = true })
+			end, { noremap = true, silent = true, desc = "Toggle Floating Debug Console" })
 		end,
+	},
+
+	{
+		"theHamsta/nvim-dap-virtual-text",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-dap-virtual-text").setup()
+		end,
+	},
+
+	{
+		"williamboman/mason.nvim", -- Mason 负责管理 LSP、DAP、格式化工具等
+		event = "VeryLazy",
+		opts = {
+			ui = {
+				border = "rounded",
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗"
+				}
+			}
+		}
+	},
+
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		event = "VeryLazy",
+		dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
+		opts = {
+			automatic_installation = true,
+			ensure_installed = { "codelldb" },
+			handlers = {}
+		}
 	},
 }
