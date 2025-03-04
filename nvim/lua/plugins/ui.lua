75aaa66e91b27551eeb0e9837e2e5af4565aee09
huzch <huzch123@gmail.com>
2025-03-04 20:04:14 +0800

    增加nvim浮动终端

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -52,6 +52,30 @@ return {
 				show_help = '<f1>',
 			},
 		},
+	},
+
+	{
+		'akinsho/toggleterm.nvim',
+		version = "*",
+		config = function()
+			require("toggleterm").setup({
+				size = 20, -- 终端窗口的大小
+				open_mapping = [[<C-\>]], -- 绑定打开/关闭终端的快捷键
+				hide_numbers = true, -- 隐藏行号
+				shade_terminals = true, -- 终端背景颜色
+				shading_factor = 2, -- 颜色深度
+				start_in_insert = true, -- 默认进入插入模式
+				insert_mappings = true, -- 允许在插入模式使用快捷键
+				terminal_mappings = true, -- 允许在终端模式使用快捷键
+				direction = "float", -- 终端方向，可选："horizontal"、"vertical"、"float"、"tab"
+				close_on_exit = true, -- 终端进程退出时自动关闭
+				shell = vim.o.shell, -- 使用 Neovim 的默认 shell
+				float_opts = {
+					border = "curved", -- 边框样式，可选："single"、"double"、"shadow"、"curved"
+					winblend = 30, -- 透明度
+				},
+			})
+		end
 	}
 
 	-- {
