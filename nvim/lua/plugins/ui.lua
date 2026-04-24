b62776283dc268d55f16307c2ef6b707e741d685
huzichang <huzichang@bytedance.com>
2026-04-24 17:08:50 +0800

    lazygit集成

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -100,4 +100,21 @@ return {
 			})
 		end
 	},
+
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
 }
